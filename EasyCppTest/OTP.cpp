1ac0717649da08f47c6f8bf65e66c22a65676aca
Dominik Thalhammer <dominik@thalhammer.it>
2016-07-29 18:03:36 +0200

    Added TOTP implementation

--- EasyCppTest/OTP.cpp
+++ EasyCppTest/OTP.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <Hash/HOTP.h>
+#include <Hash/TOTP.h>
 
 using namespace EasyCpp::Hash;
 
@@ -19,5 +20,17 @@ namespace EasyCppTest
 		token8 = hotp.generate(1, 8);
 		ASSERT_EQ(std::string("287082"), token6);
 		ASSERT_EQ(std::string("94287082"), token8);
+	}
+
+	TEST(OTP, TOTP)
+	{
+		std::string secret = "3132333435363738393031323334353637383930";
+		auto totp = TOTP(secret);
+
+		std::vector<std::string> keys;
+		for (int i = -5; i < 6; i++)
+			keys.push_back(totp.generate(i));
+
+		// Set a break point here and check using your TOTP device !
 	}
 }
