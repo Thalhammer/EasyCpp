709308ec2a2107501d5f9358a1df4dc68d999272
Dominik Thalhammer <dominik@thalhammer.it>
2016-08-30 21:54:16 +0200

    Added filterlogger.
    #61

---
+++ EasyCpp/Logging/FilterLogger.cpp
@@ -0,0 +1,43 @@
+#include "FilterLogger.h"
+
+namespace EasyCpp
+{
+	namespace Logging
+	{
+		FilterLogger::FilterLogger(ILoggerPtr logger)
+		{
+			this->setMaxLevel(Severity::MAX);
+		}
+
+		FilterLogger::~FilterLogger()
+		{
+		}
+
+		void FilterLogger::Log(Severity severity, std::string message, Bundle context)
+		{
+			if (_enabled[severity]) {
+				_logger->Log(severity, message, context);
+			}
+		}
+
+		void FilterLogger::enableLevel(Severity severity)
+		{
+			_enabled[severity] = true;
+		}
+
+		void FilterLogger::disableLevel(Severity severity)
+		{
+			_enabled[severity] = false;
+		}
+
+		void FilterLogger::setMaxLevel(Severity severity)
+		{
+			for (int i = (int)Severity::MIN; i <= (int)severity; i++) {
+				this->enableLevel((Severity)i);
+			}
+			for (int i = (int)severity; i <= (int)Severity::MAX; i++) {
+				this->disableLevel((Severity)i);
+			}
+		}
+	}
+}
