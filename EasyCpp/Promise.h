2c04e06c6d78a1b8aa6f70a4da5332f65b7f346a
Dominik Thalhammer <dominik@thalhammer.it>
2016-10-23 00:49:05 +0200

    Quick test

--- EasyCpp/Promise.h
+++ EasyCpp/Promise.h
@@ -9,6 +9,254 @@
 namespace EasyCpp
 {
 	template<typename T>
+	class Promise;
+
+	template<>
+	class Promise<void>
+	{
+	public:
+		typedef void value_type;
+
+		Promise()
+		{
+			_shared = std::make_shared<Shared>();
+		}
+
+		void resolve()
+		{
+			_shared->resolve();
+		}
+
+		void reject(std::exception_ptr ex)
+		{
+			_shared->reject(ex);
+		}
+
+		template<typename Result>
+		Promise<Result> then(std::function<Result()> fn)
+		{
+			return _shared->then(fn);
+		}
+
+		template<typename Result>
+		Promise<Result> then(std::function<Promise<Result>()> fn)
+		{
+			return _shared->then(fn);
+		}
+
+		Promise<void> error(std::function<void(std::exception_ptr)> fn)
+		{
+			return _shared->error(fn);
+		}
+
+		void reset()
+		{
+			_shared->reset();
+		}
+	private:
+		class Shared
+		{
+		public:
+			Shared()
+			{
+				_state = State::PENDING;
+			}
+
+			void resolve()
+			{
+				std::unique_lock<std::mutex> lck(_mtx);
+				if (_state != State::PENDING)
+					throw std::runtime_error("Invalid state");
+				for (auto& e : _then)
+				{
+					e();
+				}
+				_then.clear();
+				_state = State::FULFILLED;
+			}
+
+			void reject(std::exception_ptr ex)
+			{
+				std::unique_lock<std::mutex> lck(_mtx);
+				if (_state != State::PENDING)
+					throw std::runtime_error("Invalid state");
+				_exception = ex;
+				for (auto& e : _error)
+				{
+					e(_exception);
+				}
+				_error.clear();
+				_state = State::REJECTED;
+			}
+
+			template<typename Result>
+			Promise<Result> then(std::function<Result()> fn)
+			{
+				Promise<Result> pres;
+				auto pfn = [fn, pres]() mutable {
+					try {
+						Result res = fn();
+						pres.resolve(res);
+					}
+					catch (...) {
+						pres.reject(std::current_exception());
+					}
+				};
+				std::unique_lock<std::mutex> lck(_mtx);
+				if (_state == State::PENDING) {
+					_then.push_back(pfn);
+				}
+				else {
+					pfn();
+				}
+				return pres;
+			}
+
+			Promise<void> then(std::function<void()> fn)
+			{
+				Promise<void> pres;
+				auto pfn = [fn, pres]() mutable {
+					try {
+						fn();
+						pres.resolve();
+					}
+					catch (...) {
+						pres.reject(std::current_exception());
+					}
+				};
+				std::unique_lock<std::mutex> lck(_mtx);
+				if (_state == State::PENDING) {
+					_then.push_back(pfn);
+				}
+				else {
+					pfn();
+				}
+				return pres;
+			}
+
+			template<typename Result>
+			Promise<Result> then(std::function<Promise<Result>()> fn)
+			{
+				Promise<Result> pres;
+				auto pfn = [fn, pres]() mutable {
+					try {
+						Promise<Result> res = fn();
+						res.then(std::function<void(Result&)>([pres](Result& val) mutable {
+							pres.resolve(val);
+						}));
+					}
+					catch (...) {
+						pres.reject(std::current_exception());
+					}
+				};
+				std::unique_lock<std::mutex> lck(_mtx);
+				if (_state == State::PENDING) {
+					_then.push_back(pfn);
+				}
+				else {
+					pfn();
+				}
+				return pres;
+			}
+
+			Promise<void> error(std::function<void(std::exception_ptr)> fn)
+			{
+				Promise<void> pres;
+				auto pfn = [fn, pres](std::exception_ptr val) mutable {
+					try {
+						fn(val);
+						pres.resolve();
+					}
+					catch (...) {
+						pres.reject(std::current_exception());
+					}
+				};
+				std::unique_lock<std::mutex> lck(_mtx);
+				if (_state == State::PENDING) {
+					_error.push_back(pfn);
+				}
+				else {
+					pfn(_exception);
+				}
+				return pres;
+			}
+
+			void reset()
+			{
+				std::unique_lock<std::mutex> lck(_mtx);
+				if (_state == State::PENDING) {
+					throw std::runtime_error("Invalid state");
+				}
+				_state = State::PENDING;
+				_exception = nullptr;
+				_then.clear();
+				_error.clear();
+			}
+
+		private:
+			enum class State
+			{
+				PENDING,
+				FULFILLED,
+				REJECTED
+			};
+			std::mutex _mtx;
+			std::exception_ptr _exception;
+			State _state;
+			std::vector<std::function<void()>> _then;
+			std::vector<std::function<void(std::exception_ptr)>> _error;
+		};
+		std::shared_ptr<Shared> _shared;
+	public:
+		static Promise<void> Race(std::vector<Promise<void>> promises)
+		{
+			Promise<void> res;
+			for (auto& e : promises)
+			{
+				std::shared_ptr<std::atomic<bool>> done = std::make_shared<std::atomic<bool>>(false);
+				std::function<void()> fn([res, done]() mutable {
+					if (done->exchange(true))
+						return;
+					res.resolve();
+				});
+				e.then(fn);
+				e.error(std::function<void(std::exception_ptr ex)>([res, done](auto ex) mutable {
+					if (done->exchange(true))
+						return;
+					res.reject(ex);
+				}));
+			}
+			return res;
+		}
+
+		static Promise<void> All(std::vector<Promise<void>> promises)
+		{
+			Promise<void> pres;
+			size_t done = promises.size();
+			auto fn = [pres, done]() mutable {
+				done--;
+				if (done == 0)
+				{
+					pres.resolve();
+				}
+			};
+			// Required to prevent lambda copy to use persistent variables
+			auto ptr_fn = std::make_shared<decltype(fn)>(fn);
+
+			for (auto& e : promises)
+			{
+				e.then(std::function<void()>([ptr_fn]() mutable {
+					(*ptr_fn)();
+				}));
+				e.error(std::function<void(std::exception_ptr ex)>([pres](auto ex) mutable {
+					pres.reject(ex);
+				}));
+			}
+			return pres;
+		}
+	};
+
+	template<typename T>
 	class Promise
 	{
 	public:
@@ -250,251 +498,6 @@ namespace EasyCpp
 				auto& e = promises.at(id);
 				e.then(std::function<void(T&)>([ptr_fn, id](T& val) mutable {
 					(*ptr_fn)(id, val);
-				}));
-				e.error(std::function<void(std::exception_ptr ex)>([pres](auto ex) mutable {
-					pres.reject(ex);
-				}));
-			}
-			return pres;
-		}
-	};
-
-	template<>
-	class Promise<void>
-	{
-	public:
-		typedef void value_type;
-
-		Promise()
-		{
-			_shared = std::make_shared<Shared>();
-		}
-
-		void resolve()
-		{
-			_shared->resolve();
-		}
-
-		void reject(std::exception_ptr ex)
-		{
-			_shared->reject(ex);
-		}
-
-		template<typename Result>
-		Promise<Result> then(std::function<Result()> fn)
-		{
-			return _shared->then(fn);
-		}
-
-		template<typename Result>
-		Promise<Result> then(std::function<Promise<Result>()> fn)
-		{
-			return _shared->then(fn);
-		}
-
-		Promise<void> error(std::function<void(std::exception_ptr)> fn)
-		{
-			return _shared->error(fn);
-		}
-
-		void reset()
-		{
-			_shared->reset();
-		}
-	private:
-		class Shared
-		{
-		public:
-			Shared()
-			{
-				_state = State::PENDING;
-			}
-
-			void resolve()
-			{
-				std::unique_lock<std::mutex> lck(_mtx);
-				if (_state != State::PENDING)
-					throw std::runtime_error("Invalid state");
-				for (auto& e : _then)
-				{
-					e();
-				}
-				_then.clear();
-				_state = State::FULFILLED;
-			}
-
-			void reject(std::exception_ptr ex)
-			{
-				std::unique_lock<std::mutex> lck(_mtx);
-				if (_state != State::PENDING)
-					throw std::runtime_error("Invalid state");
-				_exception = ex;
-				for (auto& e : _error)
-				{
-					e(_exception);
-				}
-				_error.clear();
-				_state = State::REJECTED;
-			}
-
-			template<typename Result>
-			Promise<Result> then(std::function<Result()> fn)
-			{
-				Promise<Result> pres;
-				auto pfn = [fn, pres]() mutable {
-					try {
-						Result res = fn();
-						pres.resolve(res);
-					}
-					catch (...) {
-						pres.reject(std::current_exception());
-					}
-				};
-				std::unique_lock<std::mutex> lck(_mtx);
-				if (_state == State::PENDING) {
-					_then.push_back(pfn);
-				}
-				else {
-					pfn();
-				}
-				return pres;
-			}
-
-			Promise<void> then(std::function<void()> fn)
-			{
-				Promise<void> pres;
-				auto pfn = [fn, pres]() mutable {
-					try {
-						fn();
-						pres.resolve();
-					}
-					catch (...) {
-						pres.reject(std::current_exception());
-					}
-				};
-				std::unique_lock<std::mutex> lck(_mtx);
-				if (_state == State::PENDING) {
-					_then.push_back(pfn);
-				}
-				else {
-					pfn();
-				}
-				return pres;
-			}
-
-			template<typename Result>
-			Promise<Result> then(std::function<Promise<Result>()> fn)
-			{
-				Promise<Result> pres;
-				auto pfn = [fn, pres]() mutable {
-					try {
-						Promise<Result> res = fn();
-						res.then(std::function<void(Result&)>([pres](Result& val) mutable {
-							pres.resolve(val);
-						}));
-					}
-					catch (...) {
-						pres.reject(std::current_exception());
-					}
-				};
-				std::unique_lock<std::mutex> lck(_mtx);
-				if (_state == State::PENDING) {
-					_then.push_back(pfn);
-				}
-				else {
-					pfn();
-				}
-				return pres;
-			}
-
-			Promise<void> error(std::function<void(std::exception_ptr)> fn)
-			{
-				Promise<void> pres;
-				auto pfn = [fn, pres](std::exception_ptr val) mutable {
-					try {
-						fn(val);
-						pres.resolve();
-					}
-					catch (...) {
-						pres.reject(std::current_exception());
-					}
-				};
-				std::unique_lock<std::mutex> lck(_mtx);
-				if (_state == State::PENDING) {
-					_error.push_back(pfn);
-				}
-				else {
-					pfn(_exception);
-				}
-				return pres;
-			}
-
-			void reset()
-			{
-				std::unique_lock<std::mutex> lck(_mtx);
-				if (_state == State::PENDING) {
-					throw std::runtime_error("Invalid state");
-				}
-				_state = State::PENDING;
-				_exception = nullptr;
-				_then.clear();
-				_error.clear();
-			}
-
-		private:
-			enum class State
-			{
-				PENDING,
-				FULFILLED,
-				REJECTED
-			};
-			std::mutex _mtx;
-			std::exception_ptr _exception;
-			State _state;
-			std::vector<std::function<void()>> _then;
-			std::vector<std::function<void(std::exception_ptr)>> _error;
-		};
-		std::shared_ptr<Shared> _shared;
-	public:
-		static Promise<void> Race(std::vector<Promise<void>> promises)
-		{
-			Promise<void> res;
-			for (auto& e : promises)
-			{
-				std::shared_ptr<std::atomic<bool>> done = std::make_shared<std::atomic<bool>>(false);
-				std::function<void()> fn([res, done]() mutable {
-					if (done->exchange(true))
-						return;
-					res.resolve();
-				});
-				e.then(fn);
-				e.error(std::function<void(std::exception_ptr ex)>([res, done](auto ex) mutable {
-					if (done->exchange(true))
-						return;
-					res.reject(ex);
-				}));
-			}
-			return res;
-		}
-
-		static Promise<void> All(std::vector<Promise<void>> promises)
-		{
-			Promise<void> pres;
-			size_t done = promises.size();
-			auto fn = [pres, done]() mutable {
-				done--;
-				if (done == 0)
-				{
-					pres.resolve();
-				}
-			};
-			// Required to prevent lambda copy to use persistent variables
-			auto ptr_fn = std::make_shared<decltype(fn)>(fn);
-
-			for (auto& e: promises)
-			{
-				e.then(std::function<void()>([ptr_fn]() mutable {
-					(*ptr_fn)();
 				}));
 				e.error(std::function<void(std::exception_ptr ex)>([pres](auto ex) mutable {
 					pres.reject(ex);
