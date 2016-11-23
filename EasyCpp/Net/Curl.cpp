ff464ee89cdac8eb807da078a6ddaddc9b75cc42
Dominik Thalhammer <thalhammer.d@googlemail.com>
2016-11-23 12:55:03 +0100

    Fix missing macro

--- EasyCpp/Net/Curl.cpp
+++ EasyCpp/Net/Curl.cpp
@@ -6,6 +6,13 @@
 #include <cstring>
 #include "../StringAlgorithm.h"
 
+// Fix for missing macro in old versions
+#ifndef CURL_AT_LEAST_VERSION
+#define CURL_VERSION_BITS(x,y,z) ((x)<<16|(y)<<8|z)
+#define CURL_AT_LEAST_VERSION(x,y,z) \
+  (LIBCURL_VERSION_NUM >= CURL_VERSION_BITS(x, y, z))
+#endif
+
 namespace EasyCpp
 {
 	namespace Net
